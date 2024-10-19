a2f2eb7864199f634818da9133a91038887949cf
Frances <delacruzfrances0720@gmail.com>
2024-10-19 17:05:53 +0800

    feat: created navigation bar

--- app/src/main/java/com/example/mywanderdiary/MainActivity.kt
+++ app/src/main/java/com/example/mywanderdiary/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mywanderdiary
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,11 +12,46 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.example.mywanderdiary.databinding.ActivityMainBinding
 import com.example.mywanderdiary.ui.theme.MyWanderDiaryTheme
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val homeFragment = HomeFragment()
+        val mapFragment = MapFragment()
+        val settingsFragment = SettingsFragment()
+
+        // set initial fragment on first load
+        setCurrentFragment(homeFragment)
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.menu_item_home -> setCurrentFragment(homeFragment)
+                R.id.menu_item_map -> setCurrentFragment(mapFragment)
+                R.id.menu_item_settings -> setCurrentFragment(settingsFragment)
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
         }
 }
